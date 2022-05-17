b63e665688467ba9a5c3f6d2fd8dd1ff15ffbda1
soyocar <royomek@gmail.com>
2022-05-16 22:33:14 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import "./styles.css";
+let btnEnv = document.getElementById("enviar");
+let num1: number = document.getElementById("dato1");
+let num2: number = document.getElementById("dato2");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+btnEnv.addEventListener("click", () => {
+  let numero1: number = Number(num1.value);
+  let numero2: number = Number(num2.value);
+  let suma, inicio, final: number;
+  if (numero1 < numero2) {
+    inicio = numero1;
+    final = numero2;
+  } else {
+    inicio = numero2;
+    final = numero1;
+    suma = 0;
+  }
+
+  for (inicio; inicio < final; inicio++) {
+    suma += inicio;
+  }
+  console.log("La suma de numeros es " + suma);
+
+  //}
+});
