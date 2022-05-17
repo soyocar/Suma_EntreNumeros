795a8b695edc74eb38e2c3b88c07f526d24eec18
soyocar <royomek@gmail.com>
2022-05-17 07:01:44 -0300

    committ commit

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import "./styles.css";
 let btnEnv = document.getElementById("enviar");
 let num1: number = document.getElementById("dato1");
 let num2: number = document.getElementById("dato2");
+let suma, inicio, final: number;
 
 btnEnv.addEventListener("click", () => {
   let numero1: number = Number(num1.value);
   let numero2: number = Number(num2.value);
-  let suma, inicio, final: number;
+  suma = 0;
   if (numero1 < numero2) {
     inicio = numero1;
     final = numero2;
   } else {
     inicio = numero2;
     final = numero1;
-    suma = 0;
   }
 
-  for (inicio; inicio < final; inicio++) {
+  for (inicio; inicio <= final; inicio++) {
     suma += inicio;
   }
   console.log("La suma de numeros es " + suma);
